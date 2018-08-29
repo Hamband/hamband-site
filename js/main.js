548a139c6dade87c3d55833e19cf900b8dfdea8a
AliReza Tofighi Mohammadi <alirtofighim@gmail.com>
2018-08-30 00:50:06 +0430

    slideshow

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('#home-slideshow div').lightSlider({
+    rtl: true,
+    pager: false,
+    item: 1,
+    slideMargin: 0,
+    loop: true,
+    adaptiveHeight:true,
+    controls: false,
+    enableDrag: false,
+    enableTouch: false,
+    auto: true,
+    pause: 6000,
+    speed: 2000,
+    mode: 'fade'
+});
